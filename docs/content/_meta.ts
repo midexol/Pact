c21b0aa3716379e4966a2fc4518b36877cf0847d
Mide_xol <okunolaolamide7@gmail.com>
2026-09-09 19:55:00 +0100

    Add 404.mdx and update _meta.ts for Nextra 4

--- docs/content/_meta.ts
+++ docs/content/_meta.ts
@@ -2,5 +2,9 @@ export default {
   index: "Overview",
   "getting-started": "Getting Started",
   architecture: "Architecture",
-  api: "API Reference"
+  api: "API Reference",
+  404: {
+    type: "page",
+    display: "hidden"
+  }
 }
